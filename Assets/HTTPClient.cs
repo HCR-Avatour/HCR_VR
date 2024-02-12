c5dfff31b059af3d8a5cecac4ba9b605b45bc583
IshaanReni <ishaanreni@gmail.com>
2024-02-12 17:26:18 +0000

    checked that inputs are correctly relayed

--- Assets/HTTPClient.cs
+++ Assets/HTTPClient.cs
@@ -5,13 +5,15 @@ using System.Net.Http;
 using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.Assertions;
+using Newtonsoft.Json;
 
 public class HTTPClient : MonoBehaviour
 {
     // Instantiate one HttpClient for your application's lifetime
     private static readonly HttpClient client = new();
     /*private readonly string server_url = "http://146.169.188.207:5000/";*/
-    private readonly string server_url = "http://127.0.0.1:5000/";
+    private readonly string server_url = "http://192.168.8.198:5000/";
+
 
     private Dictionary<string, string> msg_values = new() { };
 
@@ -21,16 +23,24 @@ public class HTTPClient : MonoBehaviour
         Uri siteUri = new Uri(server_url);
         //Debug.Log(siteUri);
 
-        msg_values.Clear();
-        msg_values.Add("client_MSG_"+ msg_values.Count, msg);
+        //msg_values.Clear();
+        //msg_values.Add("content", msg);
 
         // Create content for POST
-        var content = new FormUrlEncodedContent(msg_values);
-        Debug.Log(content.ToString());
+        //var content = new FormUrlEncodedContent(msg_values);
+        //Debug.Log(content.ToString());
 
+        var payload = new Content
+        {
+            content = msg
+        };
+
+        var stringPayload = JsonConvert.SerializeObject(payload);
+        var httpContent = new StringContent(stringPayload);
+        Debug.Log(httpContent.ToString());
 
         // Send the POST request
-        var response = await client.PostAsync(siteUri, content);
+        var response = await client.PostAsync(siteUri, httpContent);
         Assert.IsTrue(response.IsSuccessStatusCode);
         Debug.Log("RESPONSE: " + response.Content);
 
@@ -39,4 +49,9 @@ public class HTTPClient : MonoBehaviour
 
     }
 
+}
+
+public class Content
+{
+    public string content { get; set; }
 }
