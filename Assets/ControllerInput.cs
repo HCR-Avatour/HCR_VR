42b0689678244100a862f1eb6b95ad2fa5134117
IshaanReni <ishaanreni@gmail.com>
2024-02-22 18:45:15 +0000

    json packaging is now working and compliant with everything

--- Assets/ControllerInput.cs
+++ Assets/ControllerInput.cs
@@ -50,13 +50,43 @@ public class ControllerInput : MonoBehaviour {
 
         if (leftController?.isValid == false || rightController?.isValid == false) updateInputDevices();
 
+        //check if both controllers are in use
+        if ((leftController is not null) && (rightController is not null))
+        {
+            // Get joystick input
+            if ((leftController.Value.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 leftJoystickInput)) && (rightController.Value.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 rightJoystickInput)))
+            {
+                // Check joystick directions
+                if ((leftJoystickInput != Vector2.zero) && (rightJoystickInput != Vector2.zero))
+                {
+                    /*Debug.Log("LEFT: Joystick input: " + leftJoystickInput + "\nRIGHT: Joystick input: " + rightJoystickInput);*/
+                    Debug.Log("LEFT: Joystick input: " + leftJoystickInput.x + ","+ leftJoystickInput.y + "\nRIGHT: Joystick input: " + rightJoystickInput.x + "," + rightJoystickInput.y);
+                    // You can use joystickInput.x and joystickInput.y to determine directions
+                    JoystickContent content = new JoystickContent();
+                    content.right_joystick_x = rightJoystickInput.x;
+                    content.right_joystick_y = rightJoystickInput.y;
+                    content.left_joystick_x = leftJoystickInput.x;
+                    content.left_joystick_y = leftJoystickInput.y;
+                    content.mode_trigger = 0;
+                    client.ClientConnect(content);
+                }
+            }
+        }
+
         // Check if the trigger button is pressed
         if (leftController is not null)
         {
             if (leftController.Value.TryGetFeatureValue(CommonUsages.triggerButton, out bool triggerPressed) && triggerPressed)
             {
                 Debug.Log("LEFT: Trigger pressed");
-                client.ClientConnect("LEFT: Trigger pressed");
+                /*client.ClientConnect("LEFT: Trigger pressed");*/
+                JoystickContent content = new JoystickContent();
+                content.right_joystick_x = 0.0f;
+                content.right_joystick_y = 0.0f;
+                content.right_joystick_x = 0.0f;
+                content.right_joystick_x = 0.0f;
+                content.mode_trigger = 1;
+                client.ClientConnect(content);
 
                 /*if (webSocketClient != null)
                 {
@@ -73,7 +103,61 @@ public class ControllerInput : MonoBehaviour {
                 {
                     Debug.Log("LEFT: Joystick input: " + joystickInput);
                     // You can use joystickInput.x and joystickInput.y to determine directions
-                    client.ClientConnect(joystickInput.ToString());
+                    /*client.ClientConnect(joystickInput.ToString());*/
+                    /*client.ClientConnect(joystickInput.x.ToString());
+                    client.ClientConnect(joystickInput.y.ToString());*/
+
+                    JoystickContent content = new JoystickContent();
+                    content.right_joystick_x = 0.0f;
+                    content.right_joystick_y = 0.0f;
+                    content.left_joystick_x = joystickInput.x;
+                    content.left_joystick_y = joystickInput.y;
+                    content.mode_trigger = 0;
+                    client.ClientConnect(content);
+
+                    /*if (webSocketClient != null)
+                    {
+                        print("inside if webSocketClient joystick");
+                        await webSocketClient.SendMessage(joystickInput.ToString());
+                    }*/
+                }
+            }
+        }
+
+        if (rightController is not null)
+        {
+            /*if (rightController.Value.TryGetFeatureValue(CommonUsages.triggerButton, out bool triggerPressed) && triggerPressed)
+            {
+                Debug.Log("RIGHT: Trigger pressed");
+                client.ClientConnect("RIGHT: Trigger pressed");
+
+                *//*if (webSocketClient != null)
+                {
+                    print("inside if webSocketClient trigger");
+                    await webSocketClient.SendMessage("LEFT trigger pressed");
+                }*//*
+            }*/
+
+            // Get joystick input
+            if (rightController.Value.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 joystickInput))
+            {
+                // Check joystick directions
+                if (joystickInput != Vector2.zero)
+                {
+                    Debug.Log("RIGHT: Joystick input: " + joystickInput);
+                    // You can use joystickInput.x and joystickInput.y to determine directions
+
+                    /*client.ClientConnect(joystickInput.ToString());*/
+                    /* client.ClientConnect(joystickInput.x.ToString());
+                     client.ClientConnect(joystickInput.y.ToString());*/
+
+                    JoystickContent content = new JoystickContent();
+                    content.left_joystick_x = 0.0f;
+                    content.left_joystick_y = 0.0f;
+                    content.right_joystick_x = joystickInput.x;
+                    content.right_joystick_y = joystickInput.y;
+                    content.mode_trigger = 0;
+                    client.ClientConnect(content);
 
                     /*if (webSocketClient != null)
                     {
--- Assets/HTTPClient.cs
+++ Assets/HTTPClient.cs
@@ -12,12 +12,12 @@ public class HTTPClient : MonoBehaviour
     // Instantiate one HttpClient for your application's lifetime
     private static readonly HttpClient client = new();
     /*private readonly string server_url = "http://146.169.188.207:5000/";*/
-    private readonly string server_url = "http://192.168.8.198:5000/";
+    private readonly string server_url = "http://172.30.36.188:5000/";
 
 
-    private Dictionary<string, string> msg_values = new() { };
+    /*private Dictionary<string, string> msg_values = new() { };*/
 
-    public async void ClientConnect(string msg)
+    public async void ClientConnect(JoystickContent msg_content)
     {
         //Debug.Log("string:"+server_url);
         Uri siteUri = new Uri(server_url);
@@ -30,13 +30,45 @@ public class HTTPClient : MonoBehaviour
         //var content = new FormUrlEncodedContent(msg_values);
         //Debug.Log(content.ToString());
 
-        var payload = new Content
+
+        /*var payload = new Content
         {
-            content = msg
+            mode_trigger = (submessages[0] != null) ? submessages[0] : "",
+            left_joystick = submessages[1],
+            right_joystick = submessages[2],
+        };*/
+
+
+        Dictionary<string, object> payload = new Dictionary<string, object>
+        {
+            { "content", new Dictionary<string, object>
+                {
+                    { "Mode", msg_content.mode_trigger },
+                    { "leftJoystick", new Dictionary<string, float>
+                        {
+                            { "x", msg_content.left_joystick_x },
+                            { "y", msg_content.left_joystick_y },
+                        }
+                    },
+                    { "rightJoystick", new Dictionary<string, float>
+                        {
+                            { "x", msg_content.right_joystick_x },
+                            { "y", msg_content.right_joystick_y },
+                        }
+                    },
+                } 
+            },
         };
 
-        var stringPayload = JsonConvert.SerializeObject(payload);
-        var httpContent = new StringContent(stringPayload);
+        // Convert the dictionary to a JSON string
+        string json = JsonConvert.SerializeObject(payload, Formatting.Indented);
+
+        // Output the JSON string
+        Debug.Log("json:" + json);
+
+        /*var stringPayload = JsonConvert.SerializeObject(payload);*/
+        /*var httpContent = new StringContent(stringPayload);*/
+        var httpContent = new StringContent(json);
         Debug.Log(httpContent.ToString());
 
         // Send the POST request
@@ -51,7 +83,11 @@ public class HTTPClient : MonoBehaviour
 
 }
 
-public class Content
+public class JoystickContent
 {
-    public string content { get; set; }
+    public int mode_trigger { get; set; }
+    public float left_joystick_x { get; set; } 
+    public float left_joystick_y { get; set; } 
+    public float right_joystick_x { get; set; }
+    public float right_joystick_y { get; set; }
 }
